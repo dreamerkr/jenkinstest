614286550f9399727f76ba841ad32658f52372c3
dreamerkr <845265283@qq.com>
2018-05-22 17:41:25 +0800

    update
    update

--- src/main/java/com/example/demo/SimpleRestController.java
+++ src/main/java/com/example/demo/SimpleRestController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.apache.log4j.Logger;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
